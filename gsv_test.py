7adb01adff1376addb4b265e3117928e35b40c7b
Huan Ning <gladcolor@gmail.com>
2020-12-09 00:13:09 -0500

    Feature: added a test function.

--- gsv_test.py
+++ gsv_test.py
@@ -1,12 +1,17 @@
 import gsv_panometa as gsv
 import os
 
+# determine a location
 lon = -77.072465
 lat = 38.985399
 
+# get the ID of the nearest panorama
 panoId = gsv.getPanoId(lon, lat)
 
+# set the saving path
 saved_path = os.getcwd()
+
+# get the depthmap
 if gsv.getDepthmap_from_panoId(panoId, saved_path):
     print(f"Processed panorama {panoId}!\nTest passed!")
 else:
