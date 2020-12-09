2f3071773ceb1369a8c5e28f901941778dc5f906
Huan Ning <gladcolor@gmail.com>
2020-12-08 23:57:47 -0500

    Feature: added a test function.

--- gsv_test.py
+++ gsv_test.py
@@ -1,3 +1,13 @@
 import gsv_panometa as gsv
+import os
 
-gsv.getPanoID(10, 20)
+lon = -77.072465
+lat = 38.985399
+
+panoId = gsv.getPanoId(lon, lat)
+
+saved_path = os.getcwd()
+if gsv.getDepthmap_from_panoId(panoId, saved_path):
+    print(f"Processed panorama {panoId}!\nTest passed!")
+else:
+    print("Test failed!")
